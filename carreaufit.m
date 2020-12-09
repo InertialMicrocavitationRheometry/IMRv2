1d2907443d5a62f4d905f74965a59aa762c03eea
Mauro Rodriguez <mrdz@caltech.edu>
2020-12-08 20:59:00 -0800

    updating the non-linear fitting procedure

--- carreaufit.m
+++ carreaufit.m
@@ -23,14 +23,14 @@ gamma = gamma(1:end);
 car = @(x,gamma) x(4) + (x(3)-x(4)).*(1+x(1)^2.*gamma.^2).^((x(2)-1)/2);
 % setting initial condition in the guess
 % setting lower and upper bounds for the fitting
-lb = [1   , 0.2, 0.5*mu0 , 0.5*mu8 ];
-ub = [100 , 0.6, 2.0*mu0 , 2*mu8   ];
+lb = [0.2    ,  0.3, 0.2*mu0 , 0.2*mu8 ];
+ub = [500   ,   0.6, 5*mu0   , 5*mu8   ];
 x0 = (lb+ub)*0.5;
 options = optimoptions('lsqcurvefit','Algorithm','trust-region-reflective',...
     'OptimalityTolerance',1e-8,'FunctionTolerance',1E-8);
 % running least-squares fit in a nonliner sense, it is still linear
 % the arguments in xsol are the values in line 22
-[xsol,resnorm] = lsqcurvefit(car,x0,gamma,mu,lb,ub,options);
+[xsol,resnorm] = lsqcurvefit(car,x0,gamma,mu,lb,ub,options)
 
 % plotting the comparisons in a log-log plot to see the comparison more
 % fairly, add this figure to the manuscript (after making it prettier)
@@ -54,16 +54,7 @@ ylabel('$\mu$ [Pa$\cdot$s]', 'Interpreter', 'Latex', 'FontSize', 20);
 box on;
 set(gcf,'color','w'); %Changes background to white 
 set(gca, 'FontName', 'Times', 'FontSize', 20); 
-ax = gca;
-outerpos = ax.OuterPosition;
-ti = ax.TightInset; 
-left = outerpos(1) + ti(1);
-bottom = outerpos(2) + ti(2);
-ax_width = outerpos(3) - ti(1) - ti(3);
-ax_height = outerpos(4) - ti(2) - ti(4);
-ax.Position = [left bottom ax_width ax_height];
-ax.TickLength = [.03 .03];
-ax.LineWidth = 1.5;
+set(gca,'TickLabelInterpreter','latex')
 print('./fitfigs/carreauFit1', '-dpng'); 
 
 
@@ -82,55 +73,36 @@ ylabel('$\mu$ [Pa$\cdot$s]', 'Interpreter', 'Latex', 'FontSize', 20);
 box on;
 set(gcf,'color','w'); %Changes background to white 
 set(gca, 'FontName', 'Times', 'FontSize', 20); 
-ax = gca;
-outerpos = ax.OuterPosition;
-ti = ax.TightInset; 
-left = outerpos(1) + ti(1);
-bottom = outerpos(2) + ti(2);
-ax_width = outerpos(3) - ti(1) - ti(3);
-ax_height = outerpos(4) - ti(2) - ti(4);
-ax.Position = [left bottom ax_width ax_height];
-ax.TickLength = [.03 .03];
-ax.LineWidth = 1.5;
+set(gca,'TickLabelInterpreter','latex')
 print('./fitfigs/carreauFit2', '-dpng'); 
 
 
 % Skalak
-car_s = @(x,skax) x(4) + (x(3)-x(4)).*(1+x(1)^2.*skax.^2).^((x(2)-1)/2);
-lb_s = [1,0.2,0.5*mu0,0.5*mu8];
-ub_s = [100,0.6,2*mu0,2*mu8];
-x0_s = (lb+ub)*0.5;
 options = optimoptions('lsqcurvefit','Algorithm','trust-region-reflective',...
-    'OptimalityTolerance',1e-8,'FunctionTolerance',1E-8);
-[xsol_s,resnorm_s] = lsqcurvefit(car_s,x0_s,skax,skay,lb_s,ub_s,options);
-% Bird
-car_b = @(x,birox) x(4) + (x(3)-x(4)).*(1+x(1)^2.*birox.^2).^((x(2)-1)/2);
-lb_b = [1,0.2,0.5*mu0,0.5*mu8];
-ub_b = [100,0.6,2*mu0,2*mu8];
-x0_b = (lb+ub)*0.5;
+    'OptimalityTolerance',1e-10,'FunctionTolerance',1E-8);
+[xsol_s,resnorm_s] = lsqcurvefit(car,x0,skax,skay,lb,ub,options)
+
+% Biro
 options = optimoptions('lsqcurvefit','Algorithm','trust-region-reflective',...
-    'OptimalityTolerance',1e-8,'FunctionTolerance',1E-8);
-[xsol_b,resnorm_b] = lsqcurvefit(car_b,x0_b,birox,biroy,lb_b,ub_b,options);
+    'OptimalityTolerance',1e-10,'FunctionTolerance',1E-8);
+[xsol_b,resnorm_b] = lsqcurvefit(car,x0,birox,biroy,lb,ub,options)
+
 % Merrill
-car_m = @(x,merix) x(4) + (x(3)-x(4)).*(1+x(1)^2.*merix.^2).^((x(2)-1)/2);
-lb_m = [1,0.2,0.5*mu0,0.5*mu8];
-ub_m = [100,0.6,2*mu0,2*mu8];
-x0_m = (lb+ub)*0.5;
 options = optimoptions('lsqcurvefit','Algorithm','trust-region-reflective',...
-    'OptimalityTolerance',1e-8,'FunctionTolerance',1E-8);
-[xsol_m,resnorm_m] = lsqcurvefit(car_m,x0_m,merix,meriy,lb_m,ub_m,options);
+    'OptimalityTolerance',1e-10,'FunctionTolerance',1E-8);
+[xsol_m,resnorm_m] = lsqcurvefit(car,x0,merix,meriy,lb,ub,options)
 
 figure(3)
 loglog(xl,car(xsol,xl), 'k','LineWidth',2);
 hold on
 loglog(skax,skay,'or','markerfacecolor','r','LineWidth',2);
-loglog(xl,car_s(xsol_s,xl),'--r','LineWidth',2)
+loglog(xl,car(xsol_s,xl),'--r','LineWidth',2)
 
 loglog(birox,biroy,'sb','markerfacecolor','b','LineWidth',2)
-loglog(xl,car_b(xsol_b,xl),':b','LineWidth',2)
+loglog(xl,car(xsol_b,xl),':b','LineWidth',2)
 
 loglog(merix,meriy,'g^','markerfacecolor','g','LineWidth',2)
-loglog(xl,car_m(xsol_m,xl),'-.g','LineWidth',2)
+loglog(xl,car(xsol_m,xl),'-.g','LineWidth',2)
 
 %axis([10E-1 10E1 10E-2 10E-1])
 xlim([10E-2 10E1]);
@@ -142,14 +114,5 @@ ylabel('$\mu$ [Pa$\cdot$s]', 'Interpreter', 'Latex', 'FontSize', 20);
 box on;
 set(gcf,'color','w'); %Changes background to white 
 set(gca, 'FontName', 'Times', 'FontSize', 20); 
-ax = gca;
-outerpos = ax.OuterPosition;
-ti = ax.TightInset; 
-left = outerpos(1) + ti(1);
-bottom = outerpos(2) + ti(2);
-ax_width = outerpos(3) - ti(1) - ti(3);
-ax_height = outerpos(4) - ti(2) - ti(4);
-ax.Position = [left bottom ax_width ax_height];
-ax.TickLength = [.03 .03];
-ax.LineWidth = 1.5;
+set(gca,'TickLabelInterpreter','latex')
 print('./fitfigs/carreauFit3', '-dpng'); 
