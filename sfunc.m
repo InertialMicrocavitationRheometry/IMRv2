9c548f45c8a720bfc3fd6485e58fa2f1ed5467dc
Mauro Rodriguez <mrdz@caltech.edu>
2020-12-15 21:16:32 -0800

    updated values for the simulations, next up improving results by calculating the viscosiity integral

--- sfunc.m
+++ sfunc.m
@@ -1,7 +1,7 @@
 function sofr = sfunc(r,R,Rdot,N,M)
 sofr = zeros(N,M);
-    for j = 1:M
-        for i = 1:N
+    for i = 1:N
+        for j = 1:M
             if r(i,j) >=  R(i,1)
                 sofr(i,j) = -2*Rdot(i,1)*(R(i,1)^2)/((r(i,j))^3);
             else
