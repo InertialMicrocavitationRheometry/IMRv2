c9591da7cd8b8987dc820f458dbd870ab68a9a13
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2022-01-06 13:48:12 -0500

    updated length figures

--- m_nonnewt_length.m
+++ m_nonnewt_length.m
@@ -18,7 +18,7 @@ nc = linspace(0,1-1E-6,300);
 plot(nc,dbar(muu,mul,nc),'--r','LineWidth',2)
 
 xlabel('\it{$n_c$}', 'Interpreter', 'Latex', 'FontSize', 20); 
-ylabel('$\it{d}^*$', 'Interpreter', 'Latex', 'FontSize', 20); 
+ylabel('$(r_2-r_1)/\ell$', 'Interpreter', 'Latex', 'FontSize', 20); 
 %leg = legend('$\mu_{water}$','$\mu_{blood,\infty}$', '$\mu_{blood}$', '$\mu_{blood,0}$' ); 
 %set(leg,'Interpreter','latex','Location','northeast');
 %set(leg,'FontSize',18);
