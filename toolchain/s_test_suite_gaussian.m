6bc501df440b339ecb3c248067e5962d8ea250b1
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2025-10-28 22:07:15 -0400

    small fix for test suite

--- toolchain/s_test_suite_gaussian.m
+++ toolchain/s_test_suite_gaussian.m
@@ -60,7 +60,7 @@ for radial = 1:4
                         'dt',dt,...
                         'tw',tw,...
                         'pA',pA};
-                    [ttest,Rf_test] = f_imr_fd(varin{:},'Nt',50,'Mt',50);
+                    [~,Rf_test] = f_imr_fd(varin{:},'Nt',50,'Mt',50);
                     errors_fd(count) = abs(1-Rf_test(end));
                     fprintf('Test %d: L2 norm error = %.6e\n', count, errors_fd(count));
                     [~,Rs_test] = f_imr_spectral(varin{:},'Nt',12,'Mt',12);
